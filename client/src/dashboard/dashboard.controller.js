1114e9ac9b7c0fc4881878260c9ec0aa45b186b1
Djadoun Mohamed Abderrezak <dmabderrezak@gmail.com>
2016-01-22 19:42:53 +0100

    an other commit ... this time the quotation is almost working and I did lots of work in the conversion

--- client/src/dashboard/dashboard.controller.js
+++ client/src/dashboard/dashboard.controller.js
@@ -15,38 +15,102 @@
 
     }
 
-    function DashboardQuotationNewController(ExamTypeService, ExamMethodService) {
+    function DashboardQuotationNewController(ExamTypeService, ExamMethodService, $log) {
         var vm = this;
+        vm.simulateQuery = false;
+        vm.isDisabled = false;
         vm.examTypeId = undefined;
         vm.examTypes = undefined;
         vm.examTMethods = undefined;
-        ExamTypeService.examTypeList.then(function(types) {
-            vm.examTypes = types;
-            setTimeout(function() {
-                $('#quotationSelectType').material_select();
 
-            }, 200);
-
-        });
 
         vm.getExamMethods = getExamMethods;
+        vm.types = null;
+        loadAll();
+        vm.selectedExamType = null;
+        vm.searchExamType = null;
+        vm.querySearch = querySearch;
+
+        vm.selectedExams = [{}];
+
+        vm.addNewExam = addNewExam;
+        vm.removeThisExam = removeThisExam;
+
+        function removeThisExam(id) {
+            vm.selectedExams.splice(id, 1);
+
+        }
+
+        function addNewExam() {
+            vm.selectedExams.push({});
+        }
+
+        function newState(state) {
+            alert("Sorry! You'll need to create a Constituion for " + state + " first!");
+        }
 
         function getExamMethods(ext) {
             console.log(ext)
                 //vm.examTypes = types;
             ExamMethodService.getTypeMethodsList(ext).then(function(response) {
-                vm.examTMethods = response
-                console.log(vm.examTMethods);
-                setTimeout(function() {
-                    $('#quotationSelectMethod').material_select();
+                vm.examMethods = response;
+                //vm.types = response;
+                console.log(vm.examMethods);
 
-                }, 1500);
             }, function(error) {
                 console.log(error);
             });;
 
 
 
+        }
+
+        // ******************************
+        // Internal methods
+        // ******************************
+        /**
+         * Search for types... use $timeout to simulate
+         * remote dataservice call.
+         */
+        function querySearch(query) {
+            console.log(query);
+            var results = query ? vm.types.filter(createFilterFor(query)) : [];
+            console.log(results);
+            return results;
+        }
+        /**
+         * Build `types` list of key/value pairs
+         */
+        function loadAll() {
+            // var alltypes = 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware,\
+            //   Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana,\
+            //   Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana,\
+            //   Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina,\
+            //   North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina,\
+            //   South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia,\
+            //   Wisconsin, Wyoming';
+            console.log("loaaaaaaaaaaaaading");
+            ExamTypeService.examTypeList.then(function(types) {
+                vm.types = types;
+                //return types;
+                console.log(types);
+
+            });
+            // return alltypes.split(/, +/g).map(function(state) {
+            //     return {
+            //         value: state.toLowerCase(),
+            //         display: state
+            //     };
+            // });
+        }
+        /**
+         * To be updated
+         */
+        function createFilterFor(query) {
+            var lowercaseQuery = angular.lowercase(query);
+            return function filterFn(state) {
+                return (state.name.indexOf(query) === 0);
+            };
         }
 
     }
