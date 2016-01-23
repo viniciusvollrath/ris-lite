67c7c7152205e67770c770278d219eab28e882c1
Djadoun Mohamed Abderrezak <dmabderrezak@gmail.com>
2016-01-23 20:14:12 +0100

    updated the autocomplete to show all the elemets by default

--- client/src/dashboard/dashboard.controller.js
+++ client/src/dashboard/dashboard.controller.js
@@ -15,7 +15,7 @@
 
     }
 
-    function DashboardQuotationNewController(ExamTypeService, ExamMethodService, $log) {
+    function DashboardQuotationNewController(ExamTypeService, ExamMethodService) {
         var vm = this;
         vm.simulateQuery = false;
         vm.isDisabled = false;
@@ -39,110 +39,84 @@
 
         vm.setPrice = setPrice;
         vm.setTotal = setTotal;
-
+        //add the price to the selected exams list
         function setPrice() {
-            console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
             for (var i = vm.selectedExams.length - 1; i >= 0; i--) {
 
                 vm.selectedExams[i].price = vm.selectedExams[i].selectedExamMethod.price;
                 setTotal();
-                // vm.total = vm.total + vm.selectedExams[i].price;
+
 
             };
 
         }
-
+        // calculate the total price of all the selected exams on every change
         function setTotal() {
-            console.log("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
-            console.log(vm.selectedExams);
             vm.total = 0;
-
             for (var i = vm.selectedExams.length - 1; i >= 0; i--) {
-
-                // vm.selectedExams[i].price = vm.selectedExams[i].selectedExamMethod.price
                 vm.total = vm.total + vm.selectedExams[i].price;
-
             };
 
         }
-
+        // remove selected exam from list
         function removeThisExam(id) {
             vm.selectedExams.splice(id, 1);
             setTotal();
-
         }
-
+        //add new row to selected exams
         function addNewExam() {
             vm.selectedExams.push({});
-            //setTotal();
-
         }
 
-        function newState(state) {
-            alert("Sorry! You'll need to create a Constituion for " + state + " first!");
-        }
-
+        //retrive the exam methods list 
         function getExamMethods(ext, id) {
-            console.log(ext)
-                //vm.examTypes = types;
             ExamMethodService.getTypeMethodsList(ext).then(function(response) {
                 vm.selectedExams[id].examMethods = response;
-                //vm.types = response;
-                //console.log(vm.examMethods);
-
             }, function(error) {
                 console.log(error);
             });;
 
-
-
         }
 
-        // ******************************
-        // Internal methods
-        // ******************************
-        /**
-         * Search for types... use $timeout to simulate
-         * remote dataservice call.
-         */
         function querySearch(query) {
-            console.log(query);
-            var results = query ? vm.types.filter(createFilterFor(query)) : [];
-            console.log(results);
-            return results;
+            if (query != '') {
+                var results = query ? vm.types.filter(createFilterFor(query)) : [];
+                console.log(query);
+                console.log(results);
+                return results;
+            } else {
+                var results = vm.types;
+                console.log(query);
+                console.log(results);
+                return results;
+            }
         }
         /**
          * Build `types` list of key/value pairs
          */
         function loadAll() {
-            // var alltypes = 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware,\
-            //   Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana,\
-            //   Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana,\
-            //   Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina,\
-            //   North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina,\
-            //   South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia,\
-            //   Wisconsin, Wyoming';
-            console.log("loaaaaaaaaaaaaading");
             ExamTypeService.examTypeList.then(function(types) {
                 vm.types = types;
                 //return types;
-                console.log(types);
 
             });
-            // return alltypes.split(/, +/g).map(function(state) {
-            //     return {
-            //         value: state.toLowerCase(),
-            //         display: state
-            //     };
-            // });
         }
         /**
          * To be updated
          */
         function createFilterFor(query) {
-            var lowercaseQuery = angular.lowercase(query);
+            //var lowercaseQuery = angular.lowercase(query);
             return function filterFn(state) {
-                return (state.name.indexOf(query) === 0);
+                console.log(state)
+                console.log(state.name.indexOf(query) === 0)
+                if (query != '') {
+                    return (state.name.indexOf(query) === 0);
+
+                } else {
+                    return true;
+
+                }
             };
         }
 
