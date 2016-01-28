ac219024a27e19a0aee7b15cc37f36f5d60683b2
Djadoun Mohamed Abderrezak <dmabderrezak@gmail.com>
2016-01-28 16:18:21 +0100

    added condition before creating default users

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,42 +1,49 @@
 module.exports = function(server) {
-    server.models.user.create([{
-        username: 'doctor1',
-        email: 'john@doe.com',
-        password: 'doctor1',
-        rank: 'doctor',
-        role: ['admin']
-    }, {
-        username: 'admin',
-        email: 'jane@doe.com',
-        password: 'admin',
-        rank: 'admin',
-        role: ['admin']
-    }, {
-        username: 'assistant1',
-        email: 'bob@projects.com',
-        password: 'assistant1',
-        rank: 'assistant',
-        role: ['admin'],
-        role: ['assistant']
-    }], function(err, users) {
-        if (err) return console.log(err);
+    var user = server.models.user;
 
-        // Create the admin role
-        server.models.Role.create({
-            name: 'admin'
-        }, function(err, role) {
-            if (err) return console.log(err);
-            console.log(role);
-
-            // Make Bob an admin
-            role.principals.create({
-                principalType: server.models.RoleMapping.USER,
-                principalId: users[2].id
-            }, function(err, principal) {
+    user.count('', function(err, count) {
+        if (count == 0) {
+            user.create([{
+                username: 'doctor1',
+                email: 'john@doe.com',
+                password: 'doctor1',
+                rank: 'doctor',
+                role: ['admin']
+            }, {
+                username: 'admin',
+                email: 'jane@doe.com',
+                password: 'admin',
+                rank: 'admin',
+                role: ['admin']
+            }, {
+                username: 'assistant1',
+                email: 'bob@projects.com',
+                password: 'assistant1',
+                rank: 'assistant',
+                role: ['admin'],
+                role: ['assistant']
+            }], function(err, users) {
                 if (err) return console.log(err);
-                console.log(principal);
+
+                // Create the admin role
+                server.models.Role.create({
+                    name: 'admin'
+                }, function(err, role) {
+                    if (err) return console.log(err);
+                    console.log(role);
+
+                    // Make Bob an admin
+                    role.principals.create({
+                        principalType: server.models.RoleMapping.USER,
+                        principalId: users[2].id
+                    }, function(err, principal) {
+                        if (err) return console.log(err);
+                        console.log(principal);
+                    });
+                });
             });
-        });
+        };
     });
+
 
 };
