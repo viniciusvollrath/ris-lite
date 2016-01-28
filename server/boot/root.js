0bf738d95d9036e50a096a8b6fb62eedd3ba1098
Djadoun Mohamed Abderrezak <dmabderrezak@gmail.com>
2016-01-28 16:13:48 +0100

    updated the boot script by adding default user creation at boot

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,7 +1,42 @@
-// module.exports = function(server) {
-        //   // Install a `/` route that returns server status
-        //   var router = server.loopback.Router();
-        //   router.get('/', server.loopback.status());
-        //   server.use(router);
-        // };
+module.exports = function(server) {
+    server.models.user.create([{
+        username: 'doctor1',
+        email: 'john@doe.com',
+        password: 'doctor1',
+        rank: 'doctor',
+        role: ['admin']
+    }, {
+        username: 'admin',
+        email: 'jane@doe.com',
+        password: 'admin',
+        rank: 'admin',
+        role: ['admin']
+    }, {
+        username: 'assistant1',
+        email: 'bob@projects.com',
+        password: 'assistant1',
+        rank: 'assistant',
+        role: ['admin'],
+        role: ['assistant']
+    }], function(err, users) {
+        if (err) return console.log(err);
 
+        // Create the admin role
+        server.models.Role.create({
+            name: 'admin'
+        }, function(err, role) {
+            if (err) return console.log(err);
+            console.log(role);
+
+            // Make Bob an admin
+            role.principals.create({
+                principalType: server.models.RoleMapping.USER,
+                principalId: users[2].id
+            }, function(err, principal) {
+                if (err) return console.log(err);
+                console.log(principal);
+            });
+        });
+    });
+
+};
